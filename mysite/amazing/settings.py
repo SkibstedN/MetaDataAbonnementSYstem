f7510e576ae13e0e040e4f2bef136fc4ec407423
B328370 <niksk@5gzyhr3.linux-pc.sdfi.dk>
2023-12-11 15:26:22 +0100

    debug=true

--- mysite/amazing/settings.py
+++ mysite/amazing/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o4k$hb=rcb37l$&bg&m--sjm6o&h^a5n&izvhql^*4&^!8ovzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'metadataabonnement.cuddlefish.intern']
 
